edcbba3ce27bfced9108a045e08e1914189c5dd1
alahache <arnaud.lahache@gmail.com>
2012-04-24 10:04:03 +0200

    ANY & EMPTY contentSpecs

--- dtd/ContentSpec.cpp
+++ dtd/ContentSpec.cpp
@@ -9,5 +9,31 @@ namespace dtd {
 	// ------------------------------------------------------ Class ContentSpec
 	
 	ContentSpec::~ContentSpec() {}
-
+	
+	// -------------------------------------------------------------- Class Any
+	
+	Any::~Any() {}
+	
+	std::ostream& Any::put(std::ostream& out) {
+		out << "ANY";
+		return out;
+	}
+	
+	std::string Any::getRegex() {
+		return "*";
+	}
+	
+	// ------------------------------------------------------------ Class Empty
+	
+	Empty::~Empty() {}
+	
+	std::ostream& Empty::put(std::ostream& out) {
+		out << "EMPTY";
+		return out;
+	}
+	
+	std::string Empty::getRegex() {
+		return "";
+	}
+	
 }
--- dtd/ContentSpec.hpp
+++ dtd/ContentSpec.hpp
@@ -11,6 +11,20 @@ namespace dtd {
 			virtual std::ostream& put(std::ostream& out) = 0;
 			virtual std::string getRegex() = 0;
 	};
+	
+	class Any : public ContentSpec {
+		public:
+			virtual ~Any();
+			virtual std::ostream& put(std::ostream& out);
+			virtual std::string getRegex();
+	};
+	
+	class Empty : public ContentSpec {
+		public:
+			virtual ~Empty();
+			virtual std::ostream& put(std::ostream& out);
+			virtual std::string getRegex();
+	};
 }
 
 std::ostream& operator<<(std::ostream& out, dtd::ContentSpec* c);
