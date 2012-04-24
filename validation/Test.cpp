172093769e76a581caa990b79a380d8894955b99
tpatel <thibaut.patel@gmail.com>
2012-04-24 11:17:55 +0200

    Tests mis dans Test.cpp

--- validation/Test.cpp
+++ validation/Test.cpp
@@ -5,11 +5,21 @@
 #include "../dtd/Name.hpp"
 #include "../dtd/Choice.hpp"
 #include "../dtd/Seq.hpp"
+
+#include "../xml/Content.hpp"
+#include "../xml/Element.hpp"
+#include "../xml/Data.hpp"
+#include "../xml/Comment.hpp"
+#include "../xml/ProcessingInstruction.hpp"
+#include "../xml/Document.hpp"
+
 #include <iostream>
 #include <list>
 
 int main(int argc, char** argv) {
+	
 	//Création du doc dtd
+	
 	dtd::Name *n = new dtd::Name("Test");
 	dtd::Name *n2 = new dtd::Name("Test2");
 	dtd::Name *n3 = new dtd::Name("Test3");
@@ -40,9 +50,53 @@ int main(int argc, char** argv) {
 	
 	std::cout << d << std::endl;
 	
+	//Création du doc xml
+	
+	xml::Document doc;
+
+	xml::ProcessingInstruction* se = new xml::ProcessingInstruction(ElementName("","xml"));
+	AttList attributes;
+	attributes.push_back(Attribut("version", "2.0"));
+	// se->AddAttribute(Attribut("version", "2.0"));
+	se->SetAttList(&attributes);
+	doc.setXmlProlog(se);	
+
+	xml::Element* root = new xml::Element(ElementName("", "html"));
+	doc.setRoot(root);
+
+	xml::Element* body = new xml::Element(ElementName("", "body"));
+	// root->AddChild(body);
+	list<xml::Content*> childs;
+	childs.push_back(body);
+	root->SetChildren(&childs);
+
+	xml::Element* element = new xml::Element(ElementName("", "a"));
+	attributes.push_back(Attribut("alt", "Google.fr"));
+	attributes.push_back(Attribut("href", "http://www.google.fr"));	
+	element->SetAttList(&attributes);
+	// element->AddAttribute(Attribut("alt", "Google.fr"));
+	// element->AddAttribute(Attribut("href", "http://www.google.fr/"));
+	xml::Data* data = new xml::Data("Mon petit lien");
+	// element->AddChild(data);
+	childs.clear();
+	childs.push_back(data);
+
+	xml::Comment* comment = new xml::Comment("Ceci est un commentaire.");
+	// element->AddChild(comment);
+	childs.push_back(comment);
+	element->SetChildren(&childs);
+
+	childs.clear();
+	childs.push_back(element);
+	// body->AddChild(element);
+	body->SetChildren(&childs);
+
+	std::cout << doc << std::endl;
+	
+	
+	
+	
 	delete d;
-	
-	
 	
 	return 0;
 }
