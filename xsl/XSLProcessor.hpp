2fef1cfaa41b7bb61bbaa4f3f1719ee0d3d9978e
Benjamin Planche <benjamin.planche@aldream.net>
2012-04-25 22:25:27 +0300

    <Doxygen> Comments.

--- xsl/XSLProcessor.hpp
+++ xsl/XSLProcessor.hpp
@@ -1,33 +1,62 @@
 # ifndef __XSLPROCESSOR_H__
 # define __XSLPROCESSOR_H__
 
+/**
+ * @file XSLProcessor.hpp
+ * @brief XSL processing functions
+ * Contains the functions to validate a XSL stylesheet and parse XML files into HTML using it.
+ * @author Benjamin Bill Planche (Aldream)
+ */
+
+# include "Content.hpp"
+# include <string>
 #include "Document.hpp"
 
 # include <string>
 using namespace std;
 
+/**
+ * @namespace xsl
+ * Contains all classes and elements processing XSL
+ */
 namespace xsl {
-
+	
+	/**
+	 * @class XSLProcessor
+	 * @brief XSL processing functions
+	 * Object which contains the functions to validate a XSL stylesheet and parse XML files into HTML using it.
+	 */
 	class XSLProcessor 
 	{
 		private:
-			xml::Document* xslDoc; // Structure of the processed XSL file
+			xml::Document* xslDoc; /** < Structure of the processed XSL file */
 
-			string xslDTDFileName; // Name of the XSL DTD file to use to validate the XSL docs.
+			string xslDTDFileName; /** < Name of the XSL DTD file to use to validate the XSL docs. */
 
 		public:
-			// Getters and accessors
+			// Getters and setters
+			
+			/**
+			 * @brief Setter - XSL DTD file path
+			 * Sets the XSL DTD file path attribute
+			 * @param name New path.
+			 */
 			void setXslDTDFileName(string name);			
 
 			/**
-			 *	Validate and process the given XSL file.
-			 *	Return true if the XSL is conformed to the given XSL and HTML DTD, and save the document structure for the next HTML generation.
+			 * @brief Setter - XSL Validator
+			 * Validate and process the given XSL file.
+			 * @return true if the XSL is conformed to the given XSL and HTML DTD, and save the document structure for the next HTML generation.
+			 * @param xslFileName Path to the xsl file to evaluate.
 			 */
 			bool processXslFile(string xslFileName);
 
 			/**
-			 *	Validate the given XML file and generate the HTML document using the previously processed XSL file.
-			 *	Return true if the XML is conformed, and save the HTML document to the given place.
+			 * @brief Setter - XML to HTML Processor
+			 * Validate the given XML file and generate the HTML document using the previously processed XSL file.
+			 * @return true if the XML is conformed, and save the HTML document to the given place.
+			 * @param xmlFileName Path to the xml file to evaluate.
+			 * @param htmlOutputFile Path to the file which will contain the HTML file.
 			 */
 			bool generateHtmlFile(string xmlFileName, string htmlOutputFile);
 
