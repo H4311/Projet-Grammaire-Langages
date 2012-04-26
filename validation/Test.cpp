bd17c4570e44a1b2dd37ea528d582b1500fc529a
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2012-04-26 15:13:10 +0200

    Modifs de signatures pour refaire fonctionner la validation

--- validation/Test.cpp
+++ validation/Test.cpp
@@ -18,81 +18,69 @@
 
 int main(int argc, char** argv) {
 	
-	//Création du doc dtd
-	
-	dtd::Name *n = new dtd::Name("Test");
-	dtd::Name *n2 = new dtd::Name("Test2");
-	dtd::Name *n3 = new dtd::Name("Test3");
-	dtd::Name *n4 = new dtd::Name("Test4");
-	
-	std::list<dtd::Children*> l;
-	l.push_back(n);
-	l.push_back(n2);
-	dtd::Choice *c = new dtd::Choice(l);
-	
-	std::list<dtd::Children*> l2;
-	l2.push_back(n3);
-	l2.push_back(c);
-	l2.push_back(n4);
-	dtd::Seq *s = new dtd::Seq(l2);
-	
-	dtd::Element *e = new dtd::Element("html", s);
-	
-	std::list<std::string> att;
-	att.push_back("att1");
-	att.push_back("att2");
-	dtd::Attribute *a = new dtd::Attribute("AttTest", att);
-	
-	std::list<dtd::Declaration*> decl;
-	decl.push_back(e);
-	decl.push_back(a);
-	dtd::Document *d = new dtd::Document(decl);
-	
-	std::cout << d << std::endl;
-	
-	//Création du doc xml
-	
 	xml::Document doc;
-
-	xml::ProcessingInstruction* se = new xml::ProcessingInstruction(ElementName("","xml"));
 	AttList attributes;
+	list<xml::Content*> childs;
+
+	xml::ProcessingInstruction* se = new xml::ProcessingInstruction;
+	se->SetName(ElementName("","xml"));
 	attributes.push_back(Attribut("version", "2.0"));
-	// se->AddAttribute(Attribut("version", "2.0"));
-	se->SetAttList(&attributes);
+	attributes.push_back(Attribut("encoding", "utf8"));
+	se->SetAttList(attributes);
 	doc.setXmlProlog(se);	
 
-	xml::Element* root = new xml::Element(ElementName("", "html"));
+	xml::Element* root = new xml::Element;
+	root->SetName(ElementName("", "html"));
 	doc.setRoot(root);
+	
+	xml::Element* head = new xml::Element;
+	head->SetName(ElementName("", "head"));
+	list<xml::Content*> headChildren;
+	xml::Element* title = new xml::Element;
+	title->SetName(ElementName("", "title"));
+	
+	list<xml::Content*> titleChild;
+	xml::Data* titleContent = new xml::Data("Bienvenue sur mon site oueb !");
+	titleChild.push_back(titleContent);
+	title->SetChildren(titleChild);
+	
+	headChildren.push_back(title);
+	head->SetChildren(headChildren);
+	childs.push_back(head);
 
-	xml::Element* body = new xml::Element(ElementName("", "body"));
-	// root->AddChild(body);
-	list<xml::Content*> childs;
+	xml::Element* body = new xml::Element;
+	body->SetName(ElementName("", "body"));
 	childs.push_back(body);
-	root->SetChildren(&childs);
+	root->SetChildren(childs);
 
-	xml::Element* element = new xml::Element(ElementName("", "a"));
+	xml::Element* element = new xml::Element;
+	element->SetName(ElementName("", "a"));
+	attributes.clear();
 	attributes.push_back(Attribut("alt", "Google.fr"));
 	attributes.push_back(Attribut("href", "http://www.google.fr"));	
-	element->SetAttList(&attributes);
-	// element->AddAttribute(Attribut("alt", "Google.fr"));
-	// element->AddAttribute(Attribut("href", "http://www.google.fr/"));
-	xml::Data* data = new xml::Data("Mon petit lien");
-	// element->AddChild(data);
+	element->SetAttList(attributes);
+	
+	xml::EmptyElement* img = new xml::EmptyElement;
+	img->SetName(ElementName("", "img"));
+	attributes.clear();
+	attributes.push_back(Attribut("src", "http://www.google.fr/img.jpg"));
+	img->SetAttList(attributes);
+
 	childs.clear();
+	childs.push_back(img);
+
+	xml::Data* data = new xml::Data("Mon petit lien");
 	childs.push_back(data);
 
-	xml::Comment* comment = new xml::Comment("Ceci est un commentaire.");
-	// element->AddChild(comment);
+	xml::Comment* comment = new xml::Comment("<!-- Ceci est un commentaire. -->");
 	childs.push_back(comment);
-	element->SetChildren(&childs);
+	element->SetChildren(childs);
 
 	childs.clear();
 	childs.push_back(element);
-	// body->AddChild(element);
-	body->SetChildren(&childs);
+	body->SetChildren(childs);
 
 	std::cout << doc << std::endl;
-	
 	
 	// Tests Validation Node
 	cout << "==== Test validationChild ====" << endl;
@@ -111,13 +99,14 @@ int main(int argc, char** argv) {
 	
 	cout << "==== Test validationDocument ====" << endl;
 	
+	/*
 	if(Validateur::validationDocument(*d, doc)) {
 		cout << "Document validé" << endl;
 	} else {
 		cout << "Document non validé" << endl;
 	}
-	
 	delete d;
+	*/
 	
 	return 0;
 }
