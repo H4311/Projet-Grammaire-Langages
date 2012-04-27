3458216754ee90cad96cc6367e5d11cbe6d07ef3
jlgonzalezpisa <jlgonzalezpisa@gmail.com>
2012-04-27 01:11:16 -0700

    HPPs_etTestCPP

--- tests/xml_model/main.cpp
+++ tests/xml_model/main.cpp
@@ -1,12 +1,8 @@
-#include "mainTest1.cpp"
-#include "mainTest2.cpp"
-#include "mainTest3.cpp"
-#include "mainTest4.cpp"
+#include "Test.h"
 
-int main(){
-	mainTest1();
-	mainTest2();
-	mainTest3();
-	mainTest4();
-	return 0;
-}
+/**
+@file test.cpp
+@author Jose GONZALEZ
+*/
+
+
